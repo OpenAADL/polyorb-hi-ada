8337f4efb6a84c92f80d3d51d1c305bcb563d166
yoogx <hugues.jerome@gmail.com>
2014-05-18 18:10:59 +0200

    * Complete proof using GNATProve GPL2014

--- src/polyorb_hi-utils.ads
+++ src/polyorb_hi-utils.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---    Copyright (C) 2007-2009 Telecom ParisTech, 2010-2013 ESA & ISAE.      --
+--    Copyright (C) 2007-2009 Telecom ParisTech, 2010-2014 ESA & ISAE.      --
 --                                                                          --
 -- PolyORB HI is free software; you  can  redistribute  it and/or modify it --
 -- under terms of the GNU General Public License as published by the Free   --
@@ -85,17 +85,28 @@ package PolyORB_HI.Utils is
 
    HI_String_Size : constant := 80;
 
-   type HI_String is record
-      S : String (1 ..HI_String_Size);
-      L : Natural;
-   end record;
+   type HI_String is private;
 
-   function To_Hi_String (S : String) return HI_String;
-   function To_String (H : HI_String) return String;
+   function To_Hi_String (S : String) return HI_String
+     with Post => (Valid (To_Hi_String'Result));
+
+   function To_String (H : HI_String) return String
+     with Pre => (Valid (H));
+
+   function Length (H : HI_String) return Natural
+     with Pre => (Valid (H));
+
+   function Valid (H : HI_String) return Boolean;
 
    function Parse_String (S : String; First : Integer; Delimiter : Character)
-                         return Integer;
-   --  XXX
+                         return Integer
+     with Pre => (First >= S'First and First <= S'Last);
+   --  XXX GNATProve GPL2014 cannot prove this, TBI
+   --            Post => ((Parse_String'Result = S'Last)
+   --                     or (Parse_String'Result in S'Range
+   --                           and then Parse_String'Result > S'First
+   --                           and then S (Parse_String'Result - 1) = Delimiter));
+   --  Return index of the character just before Delimiter, or return S'last
 
    ------------------
    -- Naming Table --
@@ -109,5 +120,23 @@ package PolyORB_HI.Utils is
 
    type Naming_Table_Type is array (Node_Type'Range)
      of PolyORB_HI.Utils.Naming_Entry;
+
+private
+
+   type HI_String is record
+      S : String (1 ..HI_String_Size);
+      L : Natural;
+      --  It is exepected L <= HI_String_Size
+      --  XXX Todo add a type invariant
+   end record;
+
+   function Length (H : HI_String) return Natural is
+      (H.L);
+
+   function Valid (H : HI_String) return Boolean is
+      (H.L <= HI_String_Size);
+
+   function To_String (H : HI_String) return String is
+      (H.S (1 .. H.L));
 
 end PolyORB_HI.Utils;
