69e25ba8ba6d427be675da7794d4fe7ee72e774f
yoogx <hugues.jerome@gmail.com>
2014-06-08 18:41:03 -0700

    * Move Unprotected_Queue to a dedicated package

--- src/polyorb_hi-unprotected_queue.ads
+++ src/polyorb_hi-unprotected_queue.ads
@@ -6,6 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
+<<<<<<< HEAD
 --                   Copyright (C) 2014-2015 ESA & ISAE.                    --
 --                                                                          --
 -- PolyORB-HI is free software; you can redistribute it and/or modify under --
@@ -23,6 +24,27 @@
 -- a copy of the GCC Runtime Library Exception along with this program;     --
 -- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
 -- <http://www.gnu.org/licenses/>.                                          --
+=======
+--                     Copyright (C) 2014 ESA & ISAE.                       --
+--                                                                          --
+-- PolyORB HI is free software; you  can  redistribute  it and/or modify it --
+-- under terms of the GNU General Public License as published by the Free   --
+-- Software Foundation; either version 2, or (at your option) any later.    --
+-- PolyORB HI is distributed  in the hope that it will be useful, but       --
+-- WITHOUT ANY WARRANTY;  without even the implied warranty of              --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General --
+-- Public License for more details. You should have received  a copy of the --
+-- GNU General Public  License  distributed with PolyORB HI; see file       --
+-- COPYING. If not, write  to the Free  Software Foundation, 51 Franklin    --
+-- Street, Fifth Floor, Boston, MA 02111-1301, USA.                         --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+>>>>>>>         * Move Unprotected_Queue to a dedicated package
 --                                                                          --
 --              PolyORB-HI/Ada is maintained by the TASTE project           --
 --                      (taste-users@lists.tuxfamily.org)                   --
@@ -31,9 +53,18 @@
 
 with Ada.Unchecked_Conversion;
 With Ada.Real_Time;
+<<<<<<< HEAD
 
 with PolyORB_HI_Generated.Deployment;
 
+=======
+With System;
+
+with PolyORB_HI_Generated.Deployment;
+
+with PolyORB_HI.Errors;
+with PolyORB_HI.Messages;
+>>>>>>>         * Move Unprotected_Queue to a dedicated package
 with PolyORB_HI.Port_Kinds;
 with PolyORB_HI.Streams;
 
@@ -53,6 +84,12 @@ generic
     PolyORB_HI_Generated.Deployment.Port_Sized_String;
    --  An array type to specify the image of each port.
 
+<<<<<<< HEAD
+=======
+   type Address_Array is array (Port_Type) of System.Address;
+   --  An array to specify a list of arrays of various sizes.
+
+>>>>>>>         * Move Unprotected_Queue to a dedicated package
    type Overflow_Protocol_Array is array (Port_Type) of
      Port_Kinds.Overflow_Handling_Protocol;
    --  An array to specify the overflow_handling_protocol of each port
@@ -108,6 +145,28 @@ generic
    --  deducing it from Thread_Fifo_Sizes is done to guarantee static
    --  allocation of the global message queue of the thread.
 
+<<<<<<< HEAD
+=======
+   N_Destinations : in Integer_Array;
+   --  For each OUT port, we give the number of destinations. This
+   --  will be used to know the length of each element of the array
+   --  below.
+
+   Destinations : in Address_Array;
+   --  For each OUT port, we give the address of an constant
+   --  Entity_Type array containing the list of all the destination of
+   --  the port. For IN ports, we give Null_Address.
+
+   with procedure Marshall
+     (R :        Thread_Interface_Type;
+      M : in out PolyORB_Hi.Messages.Message_Type);
+   --  A procedure that marshalls a Thread port content into a message.
+
+   with function Next_Deadline return Ada.Real_Time.Time;
+   --  To indicate when does the next deadline of the thread occur (in
+   --  absolute time).
+
+>>>>>>>         * Move Unprotected_Queue to a dedicated package
 package PolyORB_HI.Unprotected_Queue is
 
    use Ada.Real_Time;
