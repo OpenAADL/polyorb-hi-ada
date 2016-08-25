39c17742a857d4cedc46b7297ad6efdcb079aaf2
yoogx <hugues.jerome@gmail.com>
2016-08-25 22:39:26 +0200

    * Remove useless pragma Warnings (Off)

--- src/polyorb_hi-streams.ads
+++ src/polyorb_hi-streams.ads
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 S p e c                                  --
 --                                                                          --
---    Copyright (C) 2008-2009 Telecom ParisTech, 2010-2015 ESA & ISAE.      --
+--    Copyright (C) 2008-2009 Telecom ParisTech, 2010-2016 ESA & ISAE.      --
 --                                                                          --
 -- PolyORB-HI is free software; you can redistribute it and/or modify under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -32,16 +32,16 @@
 --  This package is a reduced version of Ada.Streams without the
 --  object oriented types.
 
+with System;
+
 package PolyORB_HI.Streams is
 
    pragma Pure;
 
    --  The definition of the types above requires the use of GNAT
-   --  specific attributes 'Storage_Unit' and 'Address_Size'.
+   --  specific attribute 'Address_Size'.
 
-   pragma Warnings (Off);
-
-   type Stream_Element is mod 2 ** Standard'Storage_Unit;
+   type Stream_Element is mod 2 ** System.Storage_Unit;
 
    type Stream_Element_Offset is range
      -(2 ** (Standard'Address_Size - 1)) ..
@@ -52,7 +52,5 @@ package PolyORB_HI.Streams is
 
    type Stream_Element_Array is
      array (Stream_Element_Offset range <>) of aliased Stream_Element;
-
-   pragma Warnings (On);
 
 end PolyORB_HI.Streams;
