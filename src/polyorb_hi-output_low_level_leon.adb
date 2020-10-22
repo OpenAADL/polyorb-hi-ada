87b152f66a680783b9e36a91c9495cb46e0deb72
yoogx <hugues.jerome@gmail.com>
2020-10-21 21:25:44 -0400

    * Update copyright header
            For openaadl/ocarina#273

--- src/polyorb_hi-output_low_level_leon.adb
+++ src/polyorb_hi-output_low_level_leon.adb
@@ -6,7 +6,8 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---    Copyright (C) 2006-2009 Telecom ParisTech, 2010-2015 ESA & ISAE.      --
+--               Copyright (C) 2006-2009 Telecom ParisTech,                 --
+--                 2010-2019 ESA & ISAE, 2019-2020 OpenAADL                 --
 --                                                                          --
 -- PolyORB-HI is free software; you can redistribute it and/or modify under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -24,8 +25,8 @@
 -- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
 -- <http://www.gnu.org/licenses/>.                                          --
 --                                                                          --
---              PolyORB-HI/Ada is maintained by the TASTE project           --
---                      (taste-users@lists.tuxfamily.org)                   --
+--              PolyORB-HI/Ada is maintained by the OpenAADL team           --
+--                             (info@openaadl.org)                          --
 --                                                                          --
 ------------------------------------------------------------------------------
 
